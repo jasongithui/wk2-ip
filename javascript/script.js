98a641636577cf3611c0ab7962673e605806c1df
Jason Githui <jasongithui@gmail.comgit config --global user.name jasongithuiomit config --globalgit user.mail jasongithui@gmail.com>
2020-03-29 18:27:02 +0300

    add js code to initialize the system

--- javascript/script.js
+++ javascript/script.js
@@ -10,3 +10,26 @@ function ghanaName(){
     var days = ["Sunday", "Monday","Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     var maleNames = ["Kwasi", "Kwadwo", "Kwaku", "Kwabena", "Yaw", "Kofi", "Kwame"];
     var femaleNames = ["Akosua", "Adwoa", "Abenaa", "AKua", "Yaa", "Afua", "Ama"];
+    
+    if (year == "" || year.length < 4 || year.length > 4){
+        alert("Please enter a valid year!");
+    }
+    else if (month < 0 || month > 12 ){
+        alert("Please enter a valid month!");
+    }
+    else if( dayOfMonth < 0 || dayOfMonth >31) {
+        alert("Please enter a valid day!");
+    }
+    else if (male.checked == false && female.checked == false){
+        alert("Please enter your gender!");
+    }
+    else if (male.checked == true){
+        result.innerHTML = "Your Ghanaian name is " + maleNames[birthDay] +  " since you were born on a " + days[birthDay];
+    } 
+    else if (female.checked == true){
+        result.innerHTML = "Your Ghanaian name is " + femaleNames[birthDay] +  " since you were born on a " + days[birthDay];
+    }
+    else{
+        alert("Error")
+    }
+}
