f5b480b005fc9d113b471e8b98595e66c101ac53
Jason Githui <jasongithui@gmail.comgit config --global user.name jasongithuiomit config --globalgit user.mail jasongithui@gmail.com>
2020-03-30 04:07:50 +0300

    add background image

--- javascript/script.js
+++ javascript/script.js
@@ -10,26 +10,28 @@ function ghanaName(){
     var days = ["Sunday", "Monday","Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
     var maleNames = ["Kwasi", "Kwadwo", "Kwaku", "Kwabena", "Yaw", "Kofi", "Kwame"];
     var femaleNames = ["Akosua", "Adwoa", "Abenaa", "AKua", "Yaa", "Afua", "Ama"];
-
+    
+    
     if (year == "" || year.length < 4 || year.length > 4){
-        alert("Please enter a valid year!");
+        alert("Please enter a valid year!"); 
     }
-    else if (month < 0 || month > 12 ){
+    if (month < 1 || month > 12 ){
         alert("Please enter a valid month!");
     }
-    else if( dayOfMonth < 0 || dayOfMonth >31) {
+    if (dayOfMonth < 1 || dayOfMonth >31) {
         alert("Please enter a valid day!");
     }
-    else if (male.checked == false && female.checked == false){
-        alert("Please enter your gender!");
+    if (male.checked == false && female.checked == false){
+        alert("Please enter your gender!");     
     }
     else if (male.checked == true){
-        alert("Your Ghanaian name is " + maleNames[birthDay] +  " since you were born on a " + days[birthDay]);
+        result.innerHTML ="Your Ghanaian name is " + maleNames[birthDay] +  " since you were born on a " + days[birthDay];
     } 
     else if (female.checked == true){
         result.innerHTML = "Your Ghanaian name is " + femaleNames[birthDay] +  " since you were born on a " + days[birthDay];
-    }
-    else{
-        alert("Error")
-    }
-}
+    } 
+    else {
+        alert("Error");
+   }
+
+};
